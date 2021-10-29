929a8df7eab35485f8d7ab01423bb4556783223d
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-10-29 20:16:10 +0530

    final commit

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 const $fValue = document.getElementById('fvalue');
 const $sValue = document.getElementById('svalue');
+const $output = document.getElementById('result');
 
 var val1;
 var val2;
 $fValue.addEventListener('keypress', (e) => {
     if (e.code === 'Enter' && e.target.value != "") {
         val1 = parseInt(e.target.value);
-        console.log(val1)
+        console.log(val1);
         $fValue.value = "";
     }
 });
 $sValue.addEventListener('keypress', (e) => {
     if (e.code === 'Enter' && e.target.value != "") {
         val2 = parseInt(e.target.value);
-        console.log(val2)
+        console.log(val2);
         $sValue.value = "";
     }
 })
 
-// let result;
-// result= val1 + val2;
-// console.log(result)
-// document.getElementById('fr').innerHTML = result.innerHTML;
-
 var rslt;
 function add() {
     rslt = val1 + val2;
+    console.log(rslt);
 }
 function sub() {
-    result2 = val1 - val2;
+    rslt = val1 - val2;
+    console.log(rslt);
 }
 function mul() {
     rslt = val1 * val2;
+    console.log(rslt);
 }
 function div() {
     rslt = val1 / val2;
-}
-
-
-function res() {
-    document.getElementById("ab").innerText = rslt.innerText;
     console.log(rslt);
 }
+
+const $btt = document.getElementById('rsbtt');
+$ptag = document.createElement('p');
+
+$btt.addEventListener('click', (e) => {
+
+    if (e.target.innerText === "result" && rslt !== "") {
+
+        $ptag.innerText = `${rslt}`;
+        $output.appendChild($ptag);
+    }
+
+})
+
+const $clrbtt = document.getElementById('clrbtt');
+
+$clrbtt.addEventListener('click', (e) => {
+    console.log(e)
+    if (e.target.innerText === "clear" && rslt!= "") {
+        $output.removeChild($ptag);
+    }
+})
